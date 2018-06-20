a2d254d8c54b59cb73020f0bb65663b3bf234d7a
Marina Dargitz <marina.dargitz@gmail.com>
2018-06-20 10:11:40 -0600

    Add resize class function

--- Array.js
+++ Array.js
@@ -2,4 +2,28 @@ const Memory = require('./Memory');
 
 const fakeMem = new Memory;
 
-console.log(fakeMem);
+class Array {
+  constructor(){
+    this.length = 0;
+    this.capacity = 0;
+    this.ptr = fakeMem.allocate(this.length);
+  }
+
+  resize(size){
+    const oldPtr = this.ptr;
+    this.ptr = fakeMem.allocate(size);
+    if (this.ptr === null){
+      throw new Error('Out of memory');
+    }
+    fakeMem.copy(this.ptr, oldPtr, this.length);
+    fakeMem.free(oldPtr);
+    this.capacity = size;
+  }
+
+  push(item){
+    this.length++;
+    let newAddress = this.ptr + this.length +1;
+  }
+}
+
+module.exports = Array;
